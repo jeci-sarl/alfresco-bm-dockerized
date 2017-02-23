680c650570bc8bac611b6607e2ec8e7c5c8b5e6a
Jérémie Lesage <jeremie.lesage@jeci.fr>
2017-02-23 17:42:03 +0100

    pre-deploy app

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,17 @@ FROM tomcat:7.0.73-jre8
 MAINTAINER Jeremie Lesage <jeremie.lesage@gmail.com>
 
 ARG WAR_FILE
+ARG APP_NAME
 
 # Default Port 9080
 COPY server.xml /usr/local/tomcat/conf/server.xml
 COPY tomcat-users.xml /usr/local/tomcat/conf/tomcat-users.xml
 
-RUN rm -r /usr/local/tomcat/webapps/docs \
-    && rm -r /usr/local/tomcat/webapps/examples
-
 
 COPY $WAR_FILE /usr/local/tomcat/webapps/
+
+RUN rm -r /usr/local/tomcat/webapps/docs \
+    && rm -r /usr/local/tomcat/webapps/examples \
+    && rm -r /usr/local/tomcat/webapps/ROOT \
+    && unzip -q /usr/local/tomcat/webapps/$WAR_FILE -d /usr/local/tomcat/webapps/$APP_NAME \
+    && rm /usr/local/tomcat/webapps/$WAR_FILE
--- buildDockerImages.sh
+++ buildDockerImages.sh
@@ -5,7 +5,8 @@
 function build {
   warfile=$1
   imagename=$2
-  tagname=$3
+  tagname=`tagname $war`
+  appname=`appname $war`
 
   cat > .dockerignore<<EOF
 *.war
@@ -13,11 +14,12 @@ function build {
 EOF
 
   echo "## Building jeci/$imagename:$tagname  ##"
-  docker build -t jeci/$imagename:$tagname --build-arg WAR_FILE=$warfile .
+  sudo docker build -t jeci/$imagename:$tagname \
+    --build-arg WAR_FILE=$warfile --build-arg APP_NAME=$appname .
 
 
   echo "## Pushing jeci/$imagename:$tagname  ##"
-  docker push jeci/$imagename:$tagname;
+  #docker push jeci/$imagename:$tagname;
   rm .dockerignore
 }
 
@@ -27,17 +29,22 @@ function tagname {
   echo $R
 }
 
+function appname {
+  R="$(basename $1 .war)"
+  echo $R | grep -oE '[a-z-]+[a-z]'
+}
+
 for war in $(ls alfresco-benchmark-server-*.war)
 do
-  build $war "alfresco-bm-server" `tagname $war`
+  build $war "alfresco-bm-server"
 done
 
 for war in $(ls alfresco-benchmark-sample-*.war)
 do
-  build $war "alfresco-bm-test" `tagname $war`
+  build $war "alfresco-bm-test"
 done
 
 for war in $(ls alfresco-benchmark-tests*war)
 do
-  build $war "alfresco-bm-test" `tagname $war`
+  build $war "alfresco-bm-test"
 done
